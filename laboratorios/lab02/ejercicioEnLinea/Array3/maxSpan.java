e67438dbdc35ac69a6ab643bba5e0bff9b815640
julianramirez2 <52968528+julianramirez2@users.noreply.github.com>
2019-08-29 09:03:34 -0500

    Create maxSpan.java

---
+++ laboratorios/lab02/ejercicioEnLinea/Array3/maxSpan.java
@@ -0,0 +1,14 @@
+public int maxSpan(int[] nums) {
+  int count = 0;
+  int span = 0;
+  for(int i = 0; i<nums.length;i++){
+    for(int j = 0; j<nums.length;j++){
+      if(nums[i]==nums[j]){
+        count = j-i+1;
+        span = Math.max(count,span);
+      }
+    }
+  }
+  return span;
+}
+
