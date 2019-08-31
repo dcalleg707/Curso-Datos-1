9bc5a4c8f096f64e1d8dd82f18e27b82971cf62c
julianramirez2 <52968528+julianramirez2@users.noreply.github.com>
2019-08-31 16:07:04 -0500

    Create squareUp.java

---
+++ laboratorios/lab02/ejercicioEnLinea/Array3/squareUp.java
@@ -0,0 +1,12 @@
+public int[] squareUp(int n) {
+    int[] arr = new int[n*n];
+    if(n == 0){
+        return arr;
+    }else{
+    for(int i = n - 1; i < arr.length; i += n) {
+        for(int j = i; j >= i - i / n; j--)
+            arr[j] = i - j + 1;
+    }
+  }                            
+    return arr;
+}
