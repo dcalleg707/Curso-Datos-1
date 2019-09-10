99264ea75876e5f27ae94f0f4e8b5f0a7e9073f3
dcalleg707 <52968645+dcalleg707@users.noreply.github.com>
2019-09-10 11:27:43 -0500

    Update Punto 1.py

--- talleres/taller08/Punto 1.py
+++ talleres/taller08/Punto 1.py
@@ -1,12 +1,13 @@
 def polaco(s):
-  if len(s) <= 0:
-    return 
-  a = s.pop()
-  b = s.pop()
-  c = s.pop()
-  if c.isnumeric():
-    return eval(c + a + b)
-  else:
-    s.append(c)
-    return eval(b + a + str(polaco(s)))
+  sol = []
+  for i in range(len(s)):
+    if s[i].isnumeric():
+      sol.append(s[i])
+    else:
+      a = sol.pop()
+      b = sol.pop()
+      print(b + s[i] + a)
+      sol.append(str(eval(b + s[i] + a)))
+  return sol
+
 print(polaco(input().split(" ")))
