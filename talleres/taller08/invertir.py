fff83e24911ec3c5974e1d33188a4155e9967de2
dcalleg707 <52968645+dcalleg707@users.noreply.github.com>
2019-09-10 21:32:22 -0500

    Update invertir.py

--- talleres/taller08/invertir.py
+++ talleres/taller08/invertir.py
@@ -1,11 +1,6 @@
 def invert(s):
   sol = []
-  temp = []
-  jump = False
   for i in range(len(s)):
-    if jump:
-      jump = False
-      continue
     if s[i] == "(":
       continue
     if s[i] == ")":
@@ -19,8 +14,6 @@ def invert(s):
       sol.append(" ".join(invert(s[i+1:])))
       sol.append(s[i])
       break
-      jump = True
-
   return sol
 
 print(" ".join(invert(input().split(" "))))
