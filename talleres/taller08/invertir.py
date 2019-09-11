a896b454385246ce7cc34b3fc4ad954653eff326
dcalleg707 <52968645+dcalleg707@users.noreply.github.com>
2019-09-10 21:34:39 -0500

    Update invertir.py

--- talleres/taller08/invertir.py
+++ talleres/taller08/invertir.py
@@ -6,14 +6,11 @@ def invert(s):
     if s[i] == ")":
       continue
     if s[i].isnumeric():
-      print(sol, s[i])
       sol.append(s[i])
       continue
     elif not s[i].isnumeric():
-      print(sol, s[i])
       sol.append(" ".join(invert(s[i+1:])))
       sol.append(s[i])
       break
   return sol
-
 print(" ".join(invert(input().split(" "))))
