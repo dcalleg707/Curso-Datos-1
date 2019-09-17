303a13221e78182d7520d35d0d03d157e709af53
dcalleg707 <52968645+dcalleg707@users.noreply.github.com>
2019-09-17 10:23:21 -0500

    Add files via upload

---
+++ talleres/taller09/puntos 2 al 4.java
@@ -0,0 +1,51 @@
+public class Hash {
+    static HashMap<String, String> negocio = new HashMap<String, String>();
+    
+    public static void punto2()
+    {
+        HashMap<String, String> negocio = new HashMap<String, String>();
+        negocio.put("Google", "Estados Unidos");
+        negocio.put("La locura", "Colombia");
+        negocio.put("Nokia", "Finlandia");
+        negocio.put("Sony", "Japón");
+    }
+    
+    public static String punto3(String nombre)
+    {
+        boolean st = false;
+        for (String i : negocio.keySet()) {
+            if(i == nombre){
+                st = true;
+                break;
+            }
+        }
+        
+        if(st)
+        {
+            return negocio.get(nombre);
+        }
+        else{
+            return "No está";
+        }
+    }
+    
+    public static String punto4(String nombre)
+    {
+        boolean st = false;
+        for (String i : negocio.values()) {
+            if(i == nombre){
+                st = true;
+                break;
+            }
+        }
+        
+        if(st)
+        {
+            return negocio.get(nombre);
+        }
+        else{
+            return "No está";
+        }
+    }
+    
+}
